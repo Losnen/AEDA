1c255ceb2399715003bf738f57998443c5e430d4
Samuel Ramos Barroso <alu0100815146@ull.edu.es>
2016-04-21 21:00:09 +0000

    Práctica finalizada

--- Ordenacion/main.cpp
+++ Ordenacion/main.cpp
@@ -6,5 +6,31 @@ using namespace std;
 
 int main (void)
 {
-    Demostraciones<DNI>();
+    int opcion = 0;
+    do
+  	{
+  		cout << "Práctica 5 AEDA: Ordenación" << endl;
+	    cout << "MENU:" << endl;
+	    cout << "1 Demostracion" << endl;
+	    cout << "2 Estadistica" << endl;
+	    cout << "0 Salir"<< endl;
+	    cout << "Introduzca una opción: ";
+	    cin >> opcion;
+        switch(opcion)
+        {
+            case 0:
+	    	    exit(0);
+	        break;
+	        case 1:
+                Demostraciones<DNI>();
+	        break;
+	        case 2:
+	            Estadisticas<DNI>();
+	        break;
+	        default:
+	      	    system("clear");
+	            cout << "Opción incorrecta." << endl;
+	        break;
+	    }
+	  }while(opcion!=-1);
 }
--- Ordenacion/sort.h
+++ Ordenacion/sort.h
@@ -9,7 +9,7 @@ using namespace std;
 //Inserción
 
 template <class T>
-void Insercion(vector<T>& sec, bool demos)
+void Insercion(vector<T>& sec, bool demos, int& cnt)
 {
     for(int i = 1; i < sec.size(); i++)
     {
@@ -21,6 +21,7 @@ void Insercion(vector<T>& sec, bool demos)
         {
             sec[j] = sec[j-1];
             j--;
+            cnt++;
         }
         if(demos)
         {
@@ -44,7 +45,7 @@ void Insercion(vector<T>& sec, bool demos)
 //BubbleSort
 
 template <class T>
-void BubbleSort(vector<T>& sec, bool demos)
+void BubbleSort(vector<T>& sec, bool demos, int& cnt)
 {
     int x = 0;
     for (int i = 1; i < sec.size(); i++)
@@ -60,6 +61,7 @@ void BubbleSort(vector<T>& sec, bool demos)
                 x = sec[j-1];
                 sec[j-1] = sec[j];
                 sec[j] = x;
+                cnt++;
                 if(demos)
                 {
                     cout << "Secuencia: ";
@@ -76,7 +78,7 @@ void BubbleSort(vector<T>& sec, bool demos)
 //Funciones del SellSort
 
 template <class T>
-void deltasort(int d, vector<T>& sec, int n, bool dem)
+void deltasort(int d, vector<T>& sec, int n, bool demos, int& cnt)
 { 
     T x = 0;
     int j = 0;
@@ -89,8 +91,9 @@ void deltasort(int d, vector<T>& sec, int n, bool dem)
         {
             sec[j] = sec[j-d];
             j = (j-d);
+            cnt++;
         }
-        if(dem && (sec[j] != x))
+        if(demos)
         {
             cout << "Comparando: " << sec[j] << " " << x << endl;
         }
@@ -98,7 +101,7 @@ void deltasort(int d, vector<T>& sec, int n, bool dem)
         if(sec[j] != x)
         {
             sec[j] = x;
-            if(dem)
+            if(demos)
             {
 	            cout << "Secuencia: ";
 	            for(int k=0; k < sec.size(); k++) 
@@ -111,20 +114,20 @@ void deltasort(int d, vector<T>& sec, int n, bool dem)
 }
 
 template <class T>
-void ShellSort(vector<T>& sec, bool demos, int alpha)
+void ShellSort(vector<T>& sec, bool demos, int alpha, int& cnt)
 {
     int del = sec.size();
     while (del > alpha)
     { 
         del = del / 2 ;
-        deltasort(del,sec,sec.size(),demos);
+        deltasort(del,sec,sec.size(),demos,cnt);
     }
 }
 
 //QuickSort https://ronnyml.wordpress.com/2009/07/19/quicksort-en-c/
 
 template <class T>
-int Divide(vector<T>& sec, int ini, int fin, bool demos)
+int Divide(vector<T>& sec, int ini, int fin, bool demos, int& cnt)
 {
     int left;
     int right;
@@ -140,45 +143,62 @@ int Divide(vector<T>& sec, int ini, int fin, bool demos)
         while (sec[right] > pivot) 
         {
             right--;
+            cnt++;
         }
  
         while ((left < right) && (sec[left] <= pivot)) 
         {
             left++;
+            cnt++;
         }
- 
+        
+        
         if (left < right) 
         {
+            
             temp = sec[left];
             sec[left] = sec[right];
             sec[right] = temp;
         }
     }
- 
+    
+    if(demos)
+    {
+        cout << "Elementos comparados: " << sec[ini] << " " << sec[right] << endl;
+    }
     temp = sec[right];
     sec[right] = sec[ini];
     sec[ini] = temp;
+    
+    if(demos)
+    {
+        cout << "Secuencia tras comparación: ";
+        for(unsigned j2=0;j2<sec.size();j2++) 
+            cout << sec[j2] << " "; 
+        
+        cout << endl;
+    }
     
     return right;
 }
 
 template <class T>
-void QuickSort(vector<T>& sec, bool demos, int ini, int fin)
+void QuickSort(vector<T>& sec, bool demos, int ini, int fin, int& cnt)
 {
     int pivot;
  
     if (ini < fin) 
     {
-        pivot = Divide(sec, ini, fin, demos);
-        QuickSort(sec, demos, ini, pivot - 1);
-        QuickSort(sec, demos, pivot + 1, fin);
+        pivot = Divide(sec, ini, fin, demos,cnt);
+        QuickSort(sec, demos, ini, pivot - 1,cnt);
+        QuickSort(sec, demos, pivot + 1, fin,cnt);
     }
 }
 
 //Funciones de MergeSort
 
 template <class T>
-void mezcla(vector<T>& sec,vector<T> U,vector<T> V, bool demos)
+void mezcla(vector<T>& sec,vector<T> U,vector<T> V, bool demos, int& cnt)
 {
     int i = 0;
     int j = 0;
@@ -204,6 +224,7 @@ void mezcla(vector<T>& sec,vector<T> U,vector<T> V, bool demos)
             {
 	            sec[k] = U[i];
 	            i++;
+	            cnt++;
             }
             else
             {
@@ -242,16 +263,16 @@ void mezcla(vector<T>& sec,vector<T> U,vector<T> V, bool demos)
 }
 
 template <class T>
-void MergeSort(vector<T>& sec, bool demos)
+void MergeSort(vector<T>& sec, bool demos, int& cnt)
 {
     if(sec.size() > 1)
     {
         vector<T> U(sec.begin(),(sec.begin()+sec.size()/2));
         vector<T> V((sec.begin()+sec.size()/2), sec.end());
 
-        MergeSort(U, demos);
-        MergeSort(V, demos);
-        mezcla(sec,U,V,demos);
+        MergeSort(U, demos, cnt);
+        MergeSort(V, demos, cnt);
+        mezcla(sec,U,V,demos, cnt);
     }
 }
 
@@ -260,13 +281,87 @@ void MergeSort(vector<T>& sec, bool demos)
 template <class T>
 void Estadisticas(void)
 {
+    cout << "Modo estadística" << endl;
+    int p = 0;
+    int n = 0;
+    srand(time(NULL));
+    cout << "Introduzca el tamaño de la secuencia a ordenar: ";
+    cin >> n;
+    cout << "Introduzca el número de pruebas que se ejecutarán sobre cada método: ";
+    cin >> p;
     
+    vector< vector<T> > Estadistica(p, vector<T> (n,0));
+    
+    for(int j = 0; j < p; j++)
+    {
+        for(int k = 0; k < n; k++) 
+            Estadistica[j][k] = rand() % 30000000+50000000;
+    }
+    vector<T> aux;
+
+    vector<int> no(5,0);
+    vector<int> max(5,0);
+    vector<int> min(5,999999999);
+    vector< vector<double> > cont(5,vector<double> (p,0.0));
+    vector<double> med(5,0);
+
+    for(int i = 0; i < p; i++)
+    {
+        aux = Estadistica[i];
+
+        Insercion(aux,false,no[0]);
+        aux = Estadistica[i];
+
+        BubbleSort(aux,false, no[1]);
+        aux = Estadistica[i];
+
+        ShellSort(aux,false,0.5,no[2]);
+        aux = Estadistica[i];
+
+        QuickSort(aux,false,0,Estadistica.size()-1,no[3]);
+        aux = Estadistica[i];
+        
+        MergeSort(aux,false,no[4]);
+        aux = Estadistica[i];
+
+        for(int j = 0; j < 5; j++)
+        {
+            if(no[j] < min[j]) 
+                min[j] = no[j];
+            
+            if(no[j] > max[j]) 
+                max[j] = no[j];
+            
+            cont[j][i] = no[j];
+            no[j] = 0;
+        }
+
+    }
+
+    for(int i = 0; i < 5; i++)
+    {
+        for(int j = 0; j < p; j++)
+        {
+            med[i]+= cont[i][j];
+        }
+        med[i] = (med[i]/p);
+    }
+
+
+    cout << setw(40) << "Número de comparaciones" << endl;
+    cout << " " <<  setw(24) << "Mínimo"; cout << setw(15) << "Medio"; cout << " " << setw(15) << "Máximo" << endl;
+    cout << "Inserción" << setw(15) << min[0]; cout << setw(15) << med[0]; cout << setw(15) << max[0] << endl;
+    cout << "BubbleSort" << setw(14) << min[1]; cout << setw(15) << med[1]; cout << setw(15) << max[1] << endl;
+    cout << "ShellSort" << setw(15) << min[2]; cout << setw(15) << med[2]; cout << setw(15) << max[2] << endl;
+    cout << "QuickSort" << setw(15) << min[3]; cout << setw(15) << med[3]; cout << setw(15) << max[3] << endl;
+    cout << "MergeSort" << setw(15) << min[4]; cout << setw(15) << med[4]; cout << setw(15) << max[4] << endl;
+    cout << endl;
 }
 
 template <class T>
 void Demostraciones(void)
 {
-    
+    cout << "Modo demostracion" << endl;
     int op = -1;
     int n = 0;
     srand(time(NULL));
@@ -288,22 +383,23 @@ void Demostraciones(void)
     cout << "Elija una opción (0-4): ";
     cin >> op;
     
+    int flag = 0;
     switch(op)
     {
         case 0:
-            Insercion(Demostracion,true);
+            Insercion(Demostracion,true,flag);
         break;
         case 1:
-            BubbleSort(Demostracion,true);
+            BubbleSort(Demostracion,true,flag);
         break;
         case 2:
-            ShellSort(Demostracion,true,0.5);
+            ShellSort(Demostracion,true,0.5,flag);
         break;
         case 3:
-            QuickSort(Demostracion,true,0,Demostracion.size()-1);
+            QuickSort(Demostracion,true,0,Demostracion.size()-1,flag);
         break;
         case 4:
-            MergeSort(Demostracion,true);
+            MergeSort(Demostracion,true,flag);
         break;
     }
     
